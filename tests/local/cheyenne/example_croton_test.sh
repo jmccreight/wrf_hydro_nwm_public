b3f65f67aabe93b268eacd92ae0d67ed0357940f
James McCreight <jamesmcc@ucar.edu>
2019-10-11 22:18:03 -0600

    Change the ana and long_range namelists, small testing changes.

--- tests/local/cheyenne/example_croton_test.sh
+++ tests/local/cheyenne/example_croton_test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 ./model_test.sh \
-    -c  /glade/u/home/jamesmcc/WRF_Hydro/wrf_hydro_nwm_public \
+    -c  /glade/u/home/jamesmcc/WRF_Hydro/wrf_hydro_nwm_public_2 \
     -r  /glade/u/home/jamesmcc/WRF_Hydro/.wrf_hydro_nwm_public_REFERENCE \
     --compiler=ifort \
     --mpi=impi \
     --config='nwm_ana' \
-    --ncores=6 --queue=share \
+    --ncores=2 \
     --reference_update=false \
-    --domain_dir /glade/work/jamesmcc/domains/public/croton_NY
+    --domain_dir /glade/work/jamesmcc/domains/public/croton_NY_v2.1
 
 # Can be added
+#    --queue=share \
 #    --use_existing_test_dir
 #    --xrcmp_n_cores 4
 
